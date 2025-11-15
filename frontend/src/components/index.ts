c46a47995b5fa4417689f2a5f1bfac1b45fe3d38
Davi Souza Santos <davii.clink@gmail.com>
2025-11-14 22:15:52 -0300

    delete: removendo pasta front do repo

--- frontend/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { Button } from './Button';
-export { Input } from './Input';
-export { Select } from './Select';
-export { Modal } from './Modal';
-export { Table, TableRow, TableCell } from './Table';
-export { Loading } from './Loading';
-export { Card } from './Card';
-export { Pagination } from './Pagination';
-
